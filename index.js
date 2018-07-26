eb3702f64f3b1db326d8eb37c7c15629f2ab8754
michael <lxyamerica@gmail.com>
2018-07-26 01:15:57 -0500

    Hello from github

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
   console.log('Server received request.');
-  res.end('Hello From Azure Git');
+  res.end('Hello From Github');
 };
 
 const server = http.createServer(handler);
