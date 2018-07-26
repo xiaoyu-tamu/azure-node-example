6204b8363ce0c0e05c3acb632c891980c33138e1
michael <lxyamerica@gmail.com>
2018-07-26 01:02:02 -0500

    tweaked message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
   console.log('Server received request.');
-  res.end('Hello World');
+  res.end('Hello From Azure Git');
 };
 
 const server = http.createServer(handler);
