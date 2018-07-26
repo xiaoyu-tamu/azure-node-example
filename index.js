37be151d7b2863a2283c9513027d30786444b8e2
michael <lxyamerica@gmail.com>
2018-07-26 00:58:56 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const handler = (req, res) => {
+  console.log('Server received request.');
+  res.end('Hello World');
+};
+
+const server = http.createServer(handler);
+
+server.listen(port, (err) => {
+  if (err) console.log(err);
+  else {
+    console.log('server is up and running on port' + port);
+  }
+});
